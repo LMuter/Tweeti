f76a6e08c6d75f9f8959b8346740aaa42602ac94
Laurens <32563311+LMuter@users.noreply.github.com>
2024-08-27 05:30:14 +0200

    Delete tweeti directory

--- tweeti/labeling/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from labeling.models import Document
-
-
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('description', 'document', )
